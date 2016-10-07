3ac910e53e78cd711ee8f1d1284bf5c6cbbdccdf
Marius Mirzac <contact@enwebo.com>
2016-10-07 14:15:50 +0300

    Fixed staff area login link

--- views/loop/eggz-reservations-loop.php
+++ views/loop/eggz-reservations-loop.php
@@ -12,7 +12,12 @@
  * @subpackage Eggz_Reservations/classes/loop-views
  */
 if ( !is_user_logged_in() ) {
-	echo __('<div class="eggz-notice notice-info with-icon"><div class="notice-text">You need to be logged in to view staff area.</div><a href="/my-account/" class="eggz-btn eggz-btn-primary-outline eggz-btn-sm text-uppercase">Login</a></div>', 'eggz-reservations');
+	$myaccount_page_id = get_option( 'woocommerce_myaccount_page_id' );
+	$myaccount_page_url = '';
+	if ( $myaccount_page_id ) {
+		$myaccount_page_url = get_permalink( $myaccount_page_id );
+	}
+	echo __('<div class="eggz-notice notice-info with-icon"><div class="notice-text">You need to be logged in to view staff area.</div><a href="' . $myaccount_page_url . '" class="eggz-btn eggz-btn-primary-outline eggz-btn-sm text-uppercase">Login</a></div>', 'eggz-reservations');
 	return;
 }
 /**
